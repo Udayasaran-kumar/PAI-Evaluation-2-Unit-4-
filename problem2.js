c2f6983e2dcc79d2ed11ffb98893c762103e140b
Udayasaran-kumar <ssaran3091998@gmail.com>
2025-03-17 13:19:35 +0530

    modified

--- problem2.js
+++ problem2.js
@@ -18,7 +18,7 @@ let arr=[
   splitter(arr);
   function seperator(arr){
     let narr=[];
-    console.log(arr);
+    // console.log(arr);
     for(let i=0;i<arr.length;i++){
       for(let j=0;j<arr[0].length;j++){
         if(j%2!=0) narr.push(arr[i][j]);
