777b8b977657db74bdceb28bc5ea493cbfdca782
Udayasaran-kumar <ssaran3091998@gmail.com>
2025-03-17 13:18:01 +0530

    problem2.js added

---
+++ problem2.js
@@ -0,0 +1,28 @@
+let arr=[
+    "user1@gmail.com",
+    "user2@yahoo.com",
+    "user3@gmail.com",
+    "user4@outlook.com",
+    "user5@yahoo.com",
+    "user6@gmail.com"
+  ];
+  function splitter(arr){
+    let newArr=[];
+    
+    for(let i=0;i<arr.length;i++){
+        
+        newArr.push(arr[i].trim().split("@"));
+    }
+    seperator(newArr);
+  }
+  splitter(arr);
+  function seperator(arr){
+    let narr=[];
+    console.log(arr);
+    for(let i=0;i<arr.length;i++){
+      for(let j=0;j<arr[0].length;j++){
+        if(j%2!=0) narr.push(arr[i][j]);
+      }
+    }
+    console.log(narr);
+  }
